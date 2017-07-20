0259c6d1d77b8f68c260085f47d991e95b2bbae1
Michael Kellsy <mkellsy@mac.lan>
2017-07-19 21:21:04 -0600

    uninstall dialog

--- public/scripts/plugins.js
+++ public/scripts/plugins.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    
+    var undlg = new mdc.dialog.MDCDialog($("#uninstall-plugin-dialog")[0]);
+
+    undlg.listen("MDCDialog:accept", function () {
+        alert(undlg.plugin);
+    });
+
+    function uninstall(plugin) {
+        undlg.plugin = plugin;
+        undlg.show();
+    }
 });
