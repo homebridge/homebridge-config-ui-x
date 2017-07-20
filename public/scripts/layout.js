d78fde58f5fd5c69c0a14483deafbd181063b452
Michael Kellsy <mkellsy@mac.lan>
2017-07-19 23:10:24 -0600

    restart progress

--- public/scripts/layout.js
+++ public/scripts/layout.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-    var menu = new mdc.menu.MDCSimpleMenu($(".server-menu")[0]);
+$(document).ready(function () {
+    if ($(".server-menu").length > 0) {
+        var menu = new mdc.menu.MDCSimpleMenu($(".server-menu")[0]);
 
-    $("header").find(".server-menu-button").click(function () {
-        menu.open = !menu.open;
-    });
+        $("header").find(".server-menu-button").click(function () {
+            menu.open = !menu.open;
+        });
 
-    $(".logout-button").click(function () {
-        window.location.href = "/logout";
-    });
+        $(".logout-button").click(function () {
+            window.location.href = "/logout";
+        });
+    }
 });
