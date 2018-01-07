430c1f2f4fe26e1d30462b5973e930ae6239e867
oznu <dev@oz.nu>
2018-01-07 22:54:10 +1100

    a little cleanup

--- ui/scripts/postcss-custom-properties.js
+++ ui/scripts/postcss-custom-properties.js
@@ -1,17 +1,17 @@
-let fs = require('fs');
+let fs = require('fs')
 
-function changeFile(file, currLine, newLine) {
-  if (!fs.existsSync(file)) return;
-  let data = fs.readFileSync(file, 'utf8');
-  let result = data.replace(currLine, newLine);
+function changeFile (file, currLine, newLine) {
+  if (!fs.existsSync(file)) return
+  let data = fs.readFileSync(file, 'utf8')
+  let result = data.replace(currLine, newLine)
 
   fs.writeFile(file, result, err => {
-    if (err) console.log(err);
-  });
+    if (err) console.log(err)
+  })
 }
 
-let pkg = './node_modules/postcss-custom-properties/dist/index.js';
-let searchFileForString = 'warnings: options.warnings === undefined ? true : options.warnings,';
-let replaceFileWithString = 'warnings: options.warnings === undefined ? false : options.warnings,';
+let pkg = './node_modules/postcss-custom-properties/dist/index.js'
+let searchFileForString = 'warnings: options.warnings === undefined ? true : options.warnings,'
+let replaceFileWithString = 'warnings: options.warnings === undefined ? false : options.warnings,'
 
-changeFile(pkg, searchFileForString, replaceFileWithString);
+changeFile(pkg, searchFileForString, replaceFileWithString)
---
+++ ui/src/app/logs/logs.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+import { WsService } from '../_services/ws.service';
+
+@Component({
+  selector: 'app-logs',
+  templateUrl: './logs.component.html'
+})
+class LogsComponent implements OnInit {
+  private onOpen;
+  private onMessage;
+  private onError;
+  private term = new (<any>window).Terminal();
+
+  constructor(private ws: WsService) {}
+
+  ngOnInit() {
+    this.term.open(document.getElementById('log-output'), { focus: false });
+    this.term.fit();
+    this.term.write('\n\r\n\r\n\r\n\r\n\r');
+
+    // subscribe to log events
+    if (this.ws.socket.readyState) {
+      this.ws.send('logs-sub');
+    }
+
+    this.onOpen = this.ws.open.subscribe(() => {
+      this.ws.send('logs-sub');
+    });
+
+    this.onMessage = this.ws.message.subscribe((data) => {
+      try {
+        data = JSON.parse(data.data);
+        if (data.log) {
+          console.log(data.log)
+          this.term.write(data.log);
+        }
+      } catch (e) { }
+    });
+
+    this.onError = this.ws.error.subscribe((err) => {
+      this.term.write('Websocket failed to connect. Is the server running?\n\r');
+    });
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnDestroy() {
+    try {
+      // unsubscribe from log events
+      this.ws.send('logs-unsub');
+
+      // unsubscribe listeners
+      this.onOpen.unsubscribe();
+      this.onMessage.unsubscribe();
+      this.onError.unsubscribe();
+    } catch (e) {}
+  }
+
+}
+
+const LogsStates = {
+  name: 'logs',
+  url: '/logs',
+  component: LogsComponent
+};
+
+export { LogsComponent, LogsStates };
