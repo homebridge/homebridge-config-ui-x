260e7b824dc5c166d3e066dbabec1ae4db2cba6f
oznu <dev@oz.nu>
2018-03-03 17:52:10 +1100

    force kill log tail process when using sudo

--- src/wss/logs.ts
+++ src/wss/logs.ts
@@ -2,6 +2,7 @@ import * as os from 'os';
 import * as fs from 'fs';
 import * as pty from 'node-pty';
 import * as color from 'bash-color';
+import * as child_process from 'child_process';
 
 import { hb } from '../hb';
 
@@ -134,7 +135,14 @@ export class LogsWssHandler {
     if (this.term) {
       this.term.kill();
       this.term.destroy();
+      this.forceKillProcess();
     }
   }
 
+  forceKillProcess() {
+    if (hb.useSudo && this.term && this.term.pid) {
+      // really make sure the log tail command is killed when using sudo mode
+      child_process.exec(`sudo -n kill -9 ${this.term.pid}`);
+    }
+  }
 }
