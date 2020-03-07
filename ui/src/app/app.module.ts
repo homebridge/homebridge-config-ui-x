743a121b0d8be6e366acc179a1cbb29e1f40d736
oznu <dev@oz.nu>
2020-03-07 15:55:23 +1100

    resolve #528

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
 import { DragulaModule } from 'ng2-dragula';
 
@@ -17,6 +17,7 @@ import { RestartModule } from './modules/restart/restart.module';
 import { StatusModule } from './modules/status/status.module';
 
 import { onMonacoLoad } from './core/monaco-editor.service';
+import { supportedLocales } from './core/locales';
 
 @NgModule({
   declarations: [
@@ -58,7 +59,19 @@ import { onMonacoLoad } from './core/monaco-editor.service';
     RestartModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useFactory: (translate: TranslateService) => {
+        if (translate.currentLang in supportedLocales) {
+          return supportedLocales[translate.currentLang];
+        } else {
+          return 'en';
+        }
+      },
+      deps: [TranslateService],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ ui/src/app/core/locales.ts
@@ -0,0 +1,60 @@
+import { registerLocaleData } from '@angular/common';
+
+import localeEn from '@angular/common/locales/en';
+import localeDe from '@angular/common/locales/de';
+import localeFr from '@angular/common/locales/fr';
+import localePl from '@angular/common/locales/pl';
+import localeCs from '@angular/common/locales/cs';
+import localeRu from '@angular/common/locales/ru';
+import localeZhCn from '@angular/common/locales/zh-Hans';
+import localeZhTw from '@angular/common/locales/zh-Hant';
+import localeHu from '@angular/common/locales/hu';
+import localeJa from '@angular/common/locales/ja';
+import localeEs from '@angular/common/locales/es';
+import localeNl from '@angular/common/locales/nl';
+import localeTr from '@angular/common/locales/tr';
+import localeIt from '@angular/common/locales/it';
+import localeBg from '@angular/common/locales/bg';
+import localeSv from '@angular/common/locales/sv';
+import localeNo from '@angular/common/locales/nb';
+
+registerLocaleData(localeEn);
+registerLocaleData(localeDe);
+registerLocaleData(localeFr);
+registerLocaleData(localePl);
+registerLocaleData(localeCs);
+registerLocaleData(localeRu);
+registerLocaleData(localeZhCn);
+registerLocaleData(localeZhTw);
+registerLocaleData(localeHu);
+registerLocaleData(localeJa);
+registerLocaleData(localeEs);
+registerLocaleData(localeNl);
+registerLocaleData(localeTr);
+registerLocaleData(localeIt);
+registerLocaleData(localeBg);
+registerLocaleData(localeSv);
+registerLocaleData(localeNo);
+
+export const supportedLocales = {
+  'en': 'en',
+  'de': 'de',
+  'fr': 'fr',
+  'pl': 'pl',
+  'cs': 'cs',
+  'ru': 'ru',
+  'zh-CN': 'zh-Hans', // Chinese Simplified -> zh-cn -> zh-Hans
+  'zh-TW': 'zh-Hant', // Chinese Traditional -> zh-tw -> zh-Hant
+  'hu': 'hu',
+  'ja': 'ja',
+  'es': 'es',
+  'nl': 'nl',
+  'tr': 'tr',
+  'it': 'it',
+  'bg': 'bg',
+  'sv': 'sv',
+  'no': 'nb', // Norwegian -> no -> nb
+};
+
+
+
