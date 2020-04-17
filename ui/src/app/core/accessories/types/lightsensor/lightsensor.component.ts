7354a085189da2e5cef297f8df2ff20a4c75e0e3
NicoFR75 <nicolas@mouraud-paugam.fr>
2020-04-17 16:36:33 +0200

    Add accessories - LightSensor

---
+++ ui/src/app/core/accessories/types/lightsensor/lightsensor.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ServiceTypeX } from '../../accessories.interfaces';
+
+@Component({
+  selector: 'app-lightsensor',
+  templateUrl: './lightsensor.component.html',
+  styleUrls: ['./lightsensor.component.scss'],
+})
+export class LightsensorComponent implements OnInit {
+  @Input() public service: ServiceTypeX;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
