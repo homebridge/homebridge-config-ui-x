d769342a6d49a81d11418657238ed037ac2d8340
Donavan Becker <beckersmarthome@icloud.com>
2020-07-21 14:13:58 -0500

    Update custom-plugins.service.ts

--- ui/src/app/core/manage-plugins/custom-plugins/custom-plugins.service.ts
+++ ui/src/app/core/manage-plugins/custom-plugins/custom-plugins.service.ts
@@ -14,6 +14,7 @@ export class CustomPluginsService {
   public plugins = {
     'homebridge-gsh': HomebridgeGoogleSmarthomeComponent,
     'homebridge-honeywell-home': HomebridgeHoneywellHomeComponent,
+    'homebridge-honeywell-roomsensors': HomebridgeHoneywellHomeComponent,
     'homebridge-ring': HomebridgeRingComponent,
   };
 
