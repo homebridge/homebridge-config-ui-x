5b41a0bda7758b5299b43ef191a33adeb21f4c6b
oznu <dev@oz.nu>
2020-08-12 15:40:47 +1000

    add plugins e2e tests

---
+++ test/e2e/plugins.e2e-spec.ts
@@ -0,0 +1,138 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import { Test, TestingModule } from '@nestjs/testing';
+import { FastifyAdapter, NestFastifyApplication, } from '@nestjs/platform-fastify';
+import { AuthModule } from '../../src/core/auth/auth.module';
+import { PluginsModule } from '../../src/modules/plugins/plugins.module';
+import { HomebridgePlugin } from '../../src/modules/plugins/types';
+
+describe('PluginController (e2e)', () => {
+  let app: NestFastifyApplication;
+
+  let authFilePath: string;
+  let secretsFilePath: string;
+
+  let pluginsPath: string;
+
+  let authorization: string;
+
+  beforeAll(async () => {
+    process.env.UIX_BASE_PATH = path.resolve(__dirname, '../../');
+    process.env.UIX_STORAGE_PATH = path.resolve(__dirname, '../', '.homebridge');
+    process.env.UIX_CONFIG_PATH = path.resolve(process.env.UIX_STORAGE_PATH, 'config.json');
+    process.env.UIX_CUSTOM_PLUGIN_PATH = path.resolve(process.env.UIX_STORAGE_PATH, 'plugins');
+
+    authFilePath = path.resolve(process.env.UIX_STORAGE_PATH, 'auth.json');
+    secretsFilePath = path.resolve(process.env.UIX_STORAGE_PATH, '.uix-secrets');
+    pluginsPath = process.env.UIX_CUSTOM_PLUGIN_PATH;
+
+    // setup test config
+    await fs.copy(path.resolve(__dirname, '../mocks', 'config.json'), process.env.UIX_CONFIG_PATH);
+
+    // setup test auth file
+    await fs.copy(path.resolve(__dirname, '../mocks', 'auth.json'), authFilePath);
+    await fs.copy(path.resolve(__dirname, '../mocks', '.uix-secrets'), secretsFilePath);
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [PluginsModule, AuthModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
+    await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  beforeEach(async () => {
+    // get auth token before each test
+    authorization = 'bearer ' + (await app.inject({
+      method: 'POST',
+      path: '/auth/login',
+      payload: {
+        username: 'admin',
+        password: 'admin'
+      }
+    })).json().access_token;
+
+
+    // ensure it's clean
+    await fs.remove(pluginsPath);
+
+    // copy mock plugins
+    await fs.copy(path.resolve(__dirname, '../mocks', 'plugins'), pluginsPath, { recursive: true });
+  });
+
+  it('GET /plugins', async () => {
+    const res = await app.inject({
+      method: 'GET',
+      path: '/plugins',
+      headers: {
+        authorization,
+      }
+    });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.json().length).toBeGreaterThan(0);
+
+    const mockPlugin: HomebridgePlugin = res.json().find(x => x.name === 'homebridge-mock-plugin');
+
+    expect(mockPlugin).toBeTruthy();
+    expect(mockPlugin.settingsSchema).toEqual(true);
+  });
+
+  it('GET /plugins/search/:query (keyword)', async () => {
+    const res = await app.inject({
+      method: 'GET',
+      path: '/plugins/search/google',
+      headers: {
+        authorization,
+      }
+    });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.json().length).toBeGreaterThan(0);
+    expect(res.json().find(x => x.name === 'homebridge-gsh')).toBeTruthy();
+  });
+
+  it('GET /plugins/search/:query (exact plugin name)', async () => {
+    const res = await app.inject({
+      method: 'GET',
+      path: '/plugins/search/homebridge-daikin-esp8266',
+      headers: {
+        authorization,
+      }
+    });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.json()).toHaveLength(1);
+    expect(res.json().find(x => x.name === 'homebridge-daikin-esp8266')).toBeTruthy();
+  });
+
+
+  it('GET /plugins/config-schema/:plugin-name', async () => {
+    const res = await app.inject({
+      method: 'GET',
+      path: '/plugins/config-schema/homebridge-mock-plugin',
+      headers: {
+        authorization,
+      }
+    });
+
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('GET /plugins/changelog/:plugin-name', async () => {
+    const res = await app.inject({
+      method: 'GET',
+      path: '/plugins/changelog/homebridge-mock-plugin',
+      headers: {
+        authorization,
+      }
+    });
+
+    expect(res.statusCode).toEqual(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
