128b28107bb704be6ad2a9130d98c471433c2047
oznu <dev@oz.nu>
2021-01-11 21:59:20 +1100

    pollyfill globalThis for Node 11.x

--- src/self-check.ts
+++ src/self-check.ts
@@ -1,3 +1,11 @@
+/**
+ * Pollyfill for Node.js 11.x that does not support `globalThis`
+ * This should be removed when support for < Node.js 12 is dropped
+ */
+if (!global.globalThis && process.versions.modules === '67') {
+  (global as any).globalThis = global;
+}
+
 import * as os from 'os';
 import * as path from 'path';
 import * as child_process from 'child_process';
