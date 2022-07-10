5b615cd5648916ecd8ed7f5cbee0d840a84ff307
oznu <dev@oz.nu>
2022-07-10 19:54:16 +1000

    fix spa catch all filter

--- src/core/spa/spa.filter.ts
+++ src/core/spa/spa.filter.ts
@@ -5,16 +5,16 @@ import { Catch, NotFoundException, ExceptionFilter, HttpException, ArgumentsHost
 
 @Catch(NotFoundException)
 export class SpaFilter implements ExceptionFilter {
-  async catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const req = ctx.getRequest();
     const res = ctx.getResponse();
 
-    if (req.raw.originalUrl.startsWith('/api/') || req.raw.originalUrl.startsWith('/socket.io') || req.raw.originalUrl.startsWith('/assets')) {
+    if (req.url.startsWith('/api/') || req.url.startsWith('/socket.io') || req.url.startsWith('/assets')) {
       return res.code(404).send('Not Found');
     }
 
-    const file = await fs.readFile(path.resolve(process.env.UIX_BASE_PATH, 'public/index.html'), 'utf-8');
+    const file = fs.readFileSync(path.resolve(process.env.UIX_BASE_PATH, 'public/index.html'), 'utf-8');
     res.type('text/html');
     res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.header('Pragma', 'no-cache');
