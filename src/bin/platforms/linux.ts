0346579e0c6a763380a714bd595b226c762bd116
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2023-05-08 19:14:19 -0400

    Update linux.ts

--- src/bin/platforms/linux.ts
+++ src/bin/platforms/linux.ts
@@ -91,6 +91,7 @@ export class LinuxInstaller extends BasePlatform {
       this.hbService.logger(`Starting ${this.hbService.serviceName} Service...`);
       child_process.execSync(`systemctl start ${this.systemdServiceName}`);
       child_process.execSync(`systemctl status ${this.systemdServiceName} --no-pager`);
+
       this.hbService.logger(`${this.hbService.serviceName} Started`, 'succeed');
     } catch (e) {
       this.hbService.logger(`Failed to start ${this.hbService.serviceName}`, 'fail');
