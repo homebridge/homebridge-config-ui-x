f76a78f2681720de39f9f7c17e93374af189116c
Dave Nicolson <david.nicolson@gmail.com>
2023-09-10 21:42:38 +0200

    Fix broken links

--- src/modules/plugins/plugins.service.ts
+++ src/modules/plugins/plugins.service.ts
@@ -1252,7 +1252,7 @@ export class PluginsService {
         client.emit('stdout', `${path.resolve(cwd, 'node_modules')}\n\r\n\r`);
         client.emit('stdout', color.yellow('This may cause the operation to fail.\n\r'));
         client.emit('stdout', color.yellow('See the docs for details on how to enable sudo mode:\n\r'));
-        client.emit('stdout', color.yellow('https://github.com/homebridge/homebridge-config-ui-x#sudo-mode\n\r\n\r'));
+        client.emit('stdout', color.yellow('https://github.com/homebridge/homebridge-config-ui-x/wiki/Manual-Configuration#sudo-mode\n\r\n\r'));
       }
     }
 
