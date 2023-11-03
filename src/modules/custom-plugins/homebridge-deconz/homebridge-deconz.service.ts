a250c9220bc78d1ccb855910b63e9388a35af2fc
Ben <43026681+bwp91@users.noreply.github.com>
2023-11-03 19:03:35 +0000

    mimic hue dump file download for deconz

---
+++ src/modules/custom-plugins/homebridge-deconz/homebridge-deconz.service.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import { Injectable, NotFoundException, StreamableFile } from '@nestjs/common';
+import { ConfigService } from '../../../core/config/config.service';
+
+@Injectable()
+export class HomebridgeDeconzService {
+  constructor(
+    private configService: ConfigService,
+  ) { }
+
+  async streamDumpFile(): Promise<StreamableFile> {
+    const dumpPath = path.resolve(this.configService.storagePath, 'homebridge-deconz.json.gz');
+
+    // check file exists
+    if (!await fs.pathExists(dumpPath)) {
+      throw new NotFoundException();
+    }
+
+    // stream file to client
+    return new StreamableFile(fs.createReadStream(dumpPath));
+  }
+}
---
+++ ui/src/app/core/manage-plugins/custom-plugins/homebridge-deconz/homebridge-deconz.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
+import { saveAs } from 'file-saver';
+
+import { ApiService } from '@/app/core/api.service';
+
+@Component({
+  selector: 'app-homebridge-deconz',
+  templateUrl: './homebridge-deconz.component.html',
+  styleUrls: ['./homebridge-deconz.component.scss'],
+})
+export class HomebridgeDeconzComponent {
+
+  constructor(
+    private translate: TranslateService,
+    public $toastr: ToastrService,
+    private $api: ApiService,
+  ) { }
+
+  downloadDumpFile() {
+    this.$api.get('/plugins/custom-plugins/homebridge-deconz/dump-file', { observe: 'response', responseType: 'blob' })
+      .subscribe(
+        (res) => {
+          saveAs(res.body, 'homebridge-deconz.json.gz');
+        },
+        (err) => {
+          this.$toastr.error('Homebridge Deconz dump file does not exist yet.', this.translate.instant('toast.title_error'));
+        },
+      );
+  }
+
+}
