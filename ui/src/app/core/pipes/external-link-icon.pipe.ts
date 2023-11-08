e295e6a48726aaab106a5f643fc65645d72fb9c7
Ben <43026681+bwp91@users.noreply.github.com>
2023-11-08 07:27:22 +0000

    Update external-link-icon.pipe.ts

--- ui/src/app/core/pipes/external-link-icon.pipe.ts
+++ ui/src/app/core/pipes/external-link-icon.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ExternalLinkIconPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): unknown {
-    return value.startsWith('https://github.com') ? 'fab fa-github' : 'fas fa-fw fa-question-circle';
+    return value.startsWith('https://github.com/') ? 'fab fa-fw fa-github' : 'fas fa-fw fa-question-circle';
   }
 
 }
