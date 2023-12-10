b27f52cc1be7a8c75362689f2e282c1130514e89
Ben <43026681+bwp91@users.noreply.github.com>
2023-12-10 11:15:12 +0000

    add plugin log download

--- ui/src/app/core/manage-plugins/plugin-log-modal/plugin-log-modal.component.ts
+++ ui/src/app/core/manage-plugins/plugin-log-modal/plugin-log-modal.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import {
   Component,
   ElementRef,
@@ -7,11 +8,13 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
+import { saveAs } from 'file-saver';
 import { ToastrService } from 'ngx-toastr';
 import { Subject } from 'rxjs';
 import { ApiService } from '@/app/core/api.service';
+import { ConfirmComponent } from '@/app/core/components/confirm/confirm.component';
 import { LogService } from '@/app/core/log.service';
 
 @Component({
@@ -28,6 +31,7 @@ export class PluginLogModalComponent implements OnInit, OnDestroy {
     public activeModal: NgbActiveModal,
     private $api: ApiService,
     private $log: LogService,
+    private $modal: NgbModal,
     private $toastr: ToastrService,
     private $translate: TranslateService,
   ) { }
@@ -43,7 +47,6 @@ export class PluginLogModalComponent implements OnInit, OnDestroy {
 
   getPluginLog() {
     // Get the plugin name as configured in the config file
-    console.log(this.plugin);
     this.$api.get(`/config-editor/plugin/${encodeURIComponent(this.plugin.name)}`).subscribe(
       (result) => {
         const logAlias = this.plugin.name === 'homebridge-config-ui-x' ? 'Homebridge UI' : (result[0]?.name || this.plugin.name);
@@ -54,6 +57,57 @@ export class PluginLogModalComponent implements OnInit, OnDestroy {
         this.activeModal.dismiss();
       },
     );
+  }
+
+  downloadLogFile() {
+    const ref = this.$modal.open(ConfirmComponent);
+    ref.componentInstance.title = this.$translate.instant('logs.title_download_log_file');
+    ref.componentInstance.message = this.$translate.instant('logs.message_download_warning');
+    ref.componentInstance.confirmButtonLabel = this.$translate.instant('logs.label_download');
+    ref.componentInstance.faIconClass = 'fas fa-fw fa-user-secret primary-text';
+
+    ref.result.then(() => {
+      this.$api.get('/platform-tools/hb-service/log/download')
+        .subscribe(
+          (res: HttpResponse<any>) => {
+            if (!res.body) {
+              return;
+            }
+            const lines = res.body.split('\n');
+            let finalOutput = '';
+            let includeNextLine = false;
+
+            lines.forEach((line: string) => {
+              if (includeNextLine) {
+                if (line.match(/36m\[.*?]/)) {
+                  includeNextLine = false;
+                } else {
+                  finalOutput += line + '\r\n';
+                  return;
+                }
+              }
+
+              if (line.includes(`36m[${this.plugin.name}]`)) {
+                finalOutput += line + '\r\n';
+                includeNextLine = true;
+              }
+            });
+
+            saveAs(finalOutput, `${this.plugin.name}.log.txt`);
+          },
+          async (err: HttpErrorResponse) => {
+            let message: string;
+            try {
+              message = JSON.parse(await err.error.text()).message;
+            } catch (e) {
+              // do nothing
+            }
+            this.$toastr.error(message || 'Failed to download log file', this.$translate.instant('toast.title_error'));
+          },
+        );
+    }).catch(() => {
+      // do nothing
+    });
   }
 
   ngOnDestroy() {
