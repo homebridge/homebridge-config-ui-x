7d42de15963bc5ee54f2064455903f956edae1e0
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-08-04 16:17:58 +0200

    Update sidebar.component.ts

--- ui/src/app/shared/layout/sidebar/sidebar.component.ts
+++ ui/src/app/shared/layout/sidebar/sidebar.component.ts
@@ -56,6 +56,7 @@ export class SidebarComponent implements OnInit {
 
     // declare element for event listeners
     const sidebar = document.querySelector('.sidebar');
+    const sidebarLink = document.querySelector('.link-wrapper');
     const content = document.querySelector('.content');
 
     // expand sidebar on mouseenter
@@ -74,7 +75,7 @@ export class SidebarComponent implements OnInit {
     const touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
     document.addEventListener(touchEvent, (e: Event) => {
-      if (!sidebar.contains(e.target as HTMLElement)) {
+      if (!sidebar.contains(e.target as HTMLElement) && !sidebarLink.contains(e.target as HTMLElement)) {
         sidebar.classList.remove('expanded');
         content.classList.remove('sidebarExpanded');
       }
