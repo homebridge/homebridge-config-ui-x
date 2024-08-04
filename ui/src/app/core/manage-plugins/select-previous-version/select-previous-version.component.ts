f111c02b6c7e1c2626cee54696f0236fb1dbf684
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-08-04 12:26:37 +0200

    More pre-releases available to install (#2121)

--- ui/src/app/core/manage-plugins/select-previous-version/select-previous-version.component.ts
+++ ui/src/app/core/manage-plugins/select-previous-version/select-previous-version.component.ts
@@ -36,9 +36,14 @@ export class SelectPreviousVersionComponent implements OnInit {
           tagVersions[result.tags[key]] = key;
         }
 
-        const versions = result.versions.filter((x: any) => tagVersions[x] || !x.includes('-')).reverse();
+        // first versions with tag, then max 20 public versions, finally max 10 pre-releases (alpha or beta)
+        const versions = [
+          ...result.versions.filter((x) => tagVersions[x] ).reverse().slice(0, 3),
+          ...result.versions.filter((x) => !tagVersions[x] && !x.includes('-')).reverse().slice(0, 20),
+          ...result.versions.filter((x) => !tagVersions[x] && x.includes('-')).reverse().slice(0, 10)
+        ];
 
-        for (const version of versions.slice(0, 20)) {
+        for (const version of versions.slice(0, 33)) {
           this.versions.push({
             name: 'v' + version + (tagVersions[version] ? ' - ' + tagVersions[version] : ''),
             version,
