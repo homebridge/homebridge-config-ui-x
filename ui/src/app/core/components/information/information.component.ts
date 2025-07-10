a4a9ae484d243ef09a99923ab2341624755e5ad1
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2025-07-10 18:09:44 -0400

    Display Docker Version Details in the Update Information Box (#2488)

--- ui/src/app/core/components/information/information.component.ts
+++ ui/src/app/core/components/information/information.component.ts
@@ -1,11 +1,19 @@
 import { Component, inject, Input } from '@angular/core'
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
 import { TranslatePipe } from '@ngx-translate/core'
+import { NgxMdModule } from 'ngx-md'
+
+import { PluginsMarkdownDirective } from '@/app/core/directives/plugins.markdown.directive'
 
 @Component({
   templateUrl: './information.component.html',
+  styleUrls: ['./information.component.scss'],
   standalone: true,
-  imports: [TranslatePipe],
+  imports: [
+    TranslatePipe,
+    NgxMdModule,
+    PluginsMarkdownDirective,
+  ],
 })
 export class InformationComponent {
   private $activeModal = inject(NgbActiveModal)
@@ -17,6 +25,7 @@ export class InformationComponent {
   @Input() ctaButtonLabel?: string
   @Input() ctaButtonLink?: string
   @Input() faIconClass: string
+  @Input() markdownMessage2?: string
 
   public dismissModal() {
     this.$activeModal.dismiss('Dismiss')
