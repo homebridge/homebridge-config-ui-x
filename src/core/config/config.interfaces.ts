462d099e9f168d16ab3c5a817dca83feb89ded1f
Ben <43026681+bwp91@users.noreply.github.com>
2025-11-02 20:24:07 +0000

    add a complete https management to settings
    Co-Authored-By: Donavan Becker <beckersmarthome@icloud.com>
    
    Update it.json

--- src/core/config/config.interfaces.ts
+++ src/core/config/config.interfaces.ts
@@ -105,6 +105,8 @@ export interface HomebridgeUiConfig {
     cert?: string
     pfx?: string
     passphrase?: string
+    selfSigned?: boolean
+    selfSignedHostnames?: string[]
   }
   accessoryControl?: {
     debug?: boolean
