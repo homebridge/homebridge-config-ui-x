77d0a78c92b73e397f7d4a254a20a4396325d46f
Ben <43026681+bwp91@users.noreply.github.com>
2026-01-07 04:01:21 +0000

    a11y: improvements to custom plugins modal
    Co-Authored-By: rfiorentino1 <18695619+rfiorentino1@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ All notable changes to `homebridge-config-ui-x` will be documented in this file.
 - first step of improvements to https settings (@donavanbecker)
 - improvements for homebridge v2
 - upgrade `xterm` to `v6` + style improvements
+- a11y: improvements to custom plugins modal (from #2677) (@rfiorentino1)
 
 ### Other Changes
 
--- ui/src/app/core/manage-plugins/custom-plugins/custom-plugins.component.ts
+++ ui/src/app/core/manage-plugins/custom-plugins/custom-plugins.component.ts
@@ -180,8 +180,7 @@ export class CustomPluginsComponent implements OnInit, OnDestroy {
 
   private loadUi() {
     this.iframe = this.customPluginUiElementTarget().nativeElement as HTMLIFrameElement
-    this.iframe.src = `${environment.api.base + this.basePath
-    }/index.html?origin=${encodeURIComponent(location.origin)}&v=${encodeURIComponent(this.plugin.installedVersion)}`
+    this.iframe.src = `${environment.api.base + this.basePath}/index.html?origin=${encodeURIComponent(location.origin)}&v=${encodeURIComponent(this.plugin.installedVersion)}`
   }
 
   private handleMessage = (e: MessageEvent) => {
