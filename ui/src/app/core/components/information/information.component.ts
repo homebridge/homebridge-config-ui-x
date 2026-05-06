45e6f205c8659dcb75de0101b466e0520220a148
NorthernMan54 <19808920+NorthernMan54@users.noreply.github.com>
2026-05-05 22:04:30 -0400

    Update information.component.ts

--- ui/src/app/core/components/information/information.component.ts
+++ ui/src/app/core/components/information/information.component.ts
@@ -26,7 +26,7 @@ export class InformationComponent {
   public title = this.modalData.title
   public subtitle = this.modalData.subtitle
   public message = this.modalData.message
-  public message2 = this.modalData.markdownMessage2
+  public message2 = '' // Intentionally empty to trigger markdown rendering to resolve #2798
   public ctaButtonLabel = this.modalData.ctaButtonLabel
   public ctaButtonLink = this.modalData.ctaButtonLink
   public faIconClass = this.modalData.faIconClass
